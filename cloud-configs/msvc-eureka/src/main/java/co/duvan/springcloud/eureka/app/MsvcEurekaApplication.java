ab62a76a5e3798d2a9ba78548f1e57fe8cf696bb
DUVAN0829 <duvancasgonz29@gmail.com>
2025-08-12 09:28:27 -0500

    Modifica msvc 'eureka-sever' y msvc 'eureka-server-client'

--- cloud-configs/msvc-eureka/src/main/java/co/duvan/springcloud/eureka/app/MsvcEurekaApplication.java
+++ cloud-configs/msvc-eureka/src/main/java/co/duvan/springcloud/eureka/app/MsvcEurekaApplication.java
@@ -2,8 +2,11 @@ package co.duvan.springcloud.eureka.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsvcEurekaApplication {
 
 	public static void main(String[] args) {
